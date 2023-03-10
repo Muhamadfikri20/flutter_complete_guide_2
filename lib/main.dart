d92cbfaf5fb4d02968bc6b24eb1ba21966494894
Muhamadfikri20 <muhamadfikri9602@gmail.com>
2023-03-10 13:59:18 +0700

    [UPD]styling_text

--- lib/main.dart
+++ lib/main.dart
@@ -44,12 +44,25 @@ class MyHomePage extends StatelessWidget {
                 child: Row(children: [
                   Container(
                     margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                    decoration: BoxDecoration(border: Border.all(color: Colors.black, width: 2)),
+                    decoration: BoxDecoration(border: Border.all(color: Colors.purple, width: 2)),
                     padding: EdgeInsets.all(10),
-                    child: Text(tx.amount.toString()),
+                    child: Text(
+                      tx.amount.toString(),
+                      style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20, color: Colors.purple),
+                    ),
                   ),
                   Column(
-                    children: [Text(tx.title), Text(tx.date.toString())],
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        tx.title,
+                        style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        tx.date.toString(),
+                        style: TextStyle(color: Colors.grey),
+                      )
+                    ],
                   )
                 ]),
               );
