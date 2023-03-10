87e295ff81e5ee2fcabc2b74f44b765aa27fd4af
Muhamadfikri20 <muhamadfikri9602@gmail.com>
2023-03-10 16:15:39 +0700

    [upd]fetching_user_input

--- lib/main.dart
+++ lib/main.dart
@@ -21,6 +21,11 @@ class MyHomePage extends StatelessWidget {
     Transaction(id: 't1', title: 'New Shoes', amount: 69.99, date: DateTime.now()),
     Transaction(id: 't2', title: 'Weekly Groceries', amount: 69.53, date: DateTime.now())
   ];
+  final titleController = TextEditingController();
+  final amountController = TextEditingController();
+
+  // String titleInput;
+  // String amountInput;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -47,12 +52,22 @@ class MyHomePage extends StatelessWidget {
                     children: [
                       TextField(
                         decoration: InputDecoration(labelText: 'Title'),
+                        controller: titleController,
+                        // onChanged: (val) {
+                        //   titleInput = val;
+                        // },
                       ),
                       TextField(
                         decoration: InputDecoration(labelText: 'Amount'),
+                        controller: amountController,
+                        // onChanged: (val) => amountInput = val,
                       ),
                       FlatButton(
-                        onPressed: () {},
+                        onPressed: () {
+                          print(titleController.text);
+                          // print(titleInput);
+                          // print(amountInput);
+                        },
                         child: Text('Add Transaction'),
                         textColor: Colors.purple,
                       )
