28e513e98fe62181d0d3080c695005b28067138c
Muhamadfikri20 <muhamadfikri9602@gmail.com>
2023-03-11 11:38:16 +0700

    [UPD]connecting_widgets_and_managing_data_state

--- lib/widgets/user_transactions.dart
+++ lib/widgets/user_transactions.dart
@@ -13,10 +13,17 @@ class _UserTransactionsState extends State<UserTransactions> {
     Transaction(id: 't1', title: 'New Shoes', amount: 69.99, date: DateTime.now()),
     Transaction(id: 't2', title: 'Weekly Groceries', amount: 69.53, date: DateTime.now())
   ];
+  void _addNewTransaction(String txTitle, double txAmount) {
+    final newTx = Transaction(title: txTitle, amount: txAmount, date: DateTime.now());
+    setState(() {
+      _userTransactions.add(newTx);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
-      children: <Widget>[NewTransaction(), TransactionList()],
+      children: <Widget>[NewTransaction(), TransactionList(_userTransactions)],
     );
   }
 }
