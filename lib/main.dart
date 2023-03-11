1cb1b718d0bec514d9bcecc4d6c3ccd2e2d7b1c5
Muhamadfikri20 <muhamadfikri9602@gmail.com>
2023-03-11 10:46:56 +0700

    [UPD]splitting_the_app_into_widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
-import './transaction.dart';
 
 void main() {
   runApp(MyApp());
@@ -17,10 +15,6 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
-  final List<Transaction> transactions = [
-    Transaction(id: 't1', title: 'New Shoes', amount: 69.99, date: DateTime.now()),
-    Transaction(id: 't2', title: 'Weekly Groceries', amount: 69.53, date: DateTime.now())
-  ];
   final titleController = TextEditingController();
   final amountController = TextEditingController();
 
@@ -44,65 +38,6 @@ class MyHomePage extends StatelessWidget {
                 elevation: 5,
               ),
             ),
-            Card(
-              elevation: 5,
-              child: Container(
-                  padding: EdgeInsets.all(10),
-                  child: Column(
-                    children: [
-                      TextField(
-                        decoration: InputDecoration(labelText: 'Title'),
-                        controller: titleController,
-                        // onChanged: (val) {
-                        //   titleInput = val;
-                        // },
-                      ),
-                      TextField(
-                        decoration: InputDecoration(labelText: 'Amount'),
-                        controller: amountController,
-                        // onChanged: (val) => amountInput = val,
-                      ),
-                      FlatButton(
-                        onPressed: () {
-                          print(titleController.text);
-                          // print(titleInput);
-                          // print(amountInput);
-                        },
-                        child: Text('Add Transaction'),
-                        textColor: Colors.purple,
-                      )
-                    ],
-                  )),
-            ),
-            Column(
-                children: transactions.map((tx) {
-              return Card(
-                child: Row(children: [
-                  Container(
-                    margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                    decoration: BoxDecoration(border: Border.all(color: Colors.purple, width: 2)),
-                    padding: EdgeInsets.all(10),
-                    child: Text(
-                      '\$${tx.amount}',
-                      style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20, color: Colors.purple),
-                    ),
-                  ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        tx.title,
-                        style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                      ),
-                      Text(
-                        DateFormat.yMMMd().format(tx.date),
-                        style: TextStyle(color: Colors.grey),
-                      )
-                    ],
-                  )
-                ]),
-              );
-            }).toList()),
           ],
         ));
   }
