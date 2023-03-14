785877d2c725fe626b12dd4dcac41e475348bba9
Muhamadfikri20 <muhamadfikri9602@gmail.com>
2023-03-14 13:46:04 +0700

    further_input_and_output_styling_and_configuration

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -5,6 +5,17 @@ class NewTransaction extends StatelessWidget {
   final titleController = TextEditingController();
   final amountController = TextEditingController();
   NewTransaction(this.addTx);
+
+  void submitData() {
+    final enteredTitle = titleController.text;
+    final enteredAmount = double.parse(amountController.text);
+    if (enteredTitle.isEmpty || enteredAmount <= 0) {
+      return;
+    }
+
+    addTx(titleController.text, double.parse(amountController.text));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -17,6 +28,7 @@ class NewTransaction extends StatelessWidget {
               TextField(
                 decoration: InputDecoration(labelText: 'Title'),
                 controller: titleController,
+                onSubmitted: (_) => submitData(),
                 // onChanged: (val) {
                 //   titleInput = val;
                 // },
@@ -24,14 +36,12 @@ class NewTransaction extends StatelessWidget {
               TextField(
                 decoration: InputDecoration(labelText: 'Amount'),
                 controller: amountController,
+                keyboardType: TextInputType.number,
+                onSubmitted: (_) => submitData(),
                 // onChanged: (val) => amountInput = val,
               ),
               FlatButton(
-                onPressed: () {
-                  addTx(titleController.text, double.parse(amountController.text));
-                  // print(titleInput);
-                  // print(amountInput);
-                },
+                onPressed: submitData,
                 child: Text('Add Transaction'),
                 textColor: Colors.purple,
               )
