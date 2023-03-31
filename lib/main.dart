fa5fa59faf427b07e920a5d16283c32ba25231c1
Muhamadfikri20 <muhamadfikri9602@gmail.com>
2023-03-31 13:39:18 +0700

    [upd]understanding_the_app_lifecycle

--- lib/main.dart
+++ lib/main.dart
@@ -39,12 +39,29 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MyHomePageState extends State<MyHomePage> with WidgetsBindingObserver {
   final List<Transaction> _userTransactions = [
     // Transaction(id: 't1', title: 'New Shoes', amount: 69.99, date: DateTime.now()),
     // Transaction(id: 't2', title: 'Weekly Groceries', amount: 69.53, date: DateTime.now())
   ];
   bool _showChart = false;
+  @override
+  void initState() {
+    WidgetsBinding.instance.addObserver(this);
+    super.initState();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    print(state);
+  }
+
+  @override
+  dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
+
   List<Transaction> get _recentTransactions {
     return _userTransactions.where((tx) {
       return tx.date.isAfter(DateTime.now().subtract(Duration(days: 7)));
